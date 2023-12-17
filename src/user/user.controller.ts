7b5122cda632ba195b2afef05c36070436c397af
gexiaolong <1013363134@qq.com>
2023-12-17 18:26:53 +0800

    feat: 添加注册接口

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,7 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { UserService } from './user.service';
+import { RegisterUserDto } from './dto/register.dto';
 
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
+
+  @Post('register')
+  async register(@Body() registerUser: RegisterUserDto) {
+    this.userService.register(registerUser);
+    return 'ok';
+  }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,58 @@
-import { Injectable } from '@nestjs/common';
+import {
+  Injectable,
+  Inject,
+  HttpException,
+  HttpStatus,
+  Logger,
+} from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { User } from './entities/user.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { RegisterUserDto } from './dto/register.dto';
+import { RedisService } from '@/redis/redis.service';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  private logger = new Logger();
+
+  @InjectRepository(User)
+  private userRepository: Repository<User>;
+  @Inject(RedisService)
+  private redisService: RedisService;
+
+  async register(user: RegisterUserDto) {
+    console.log('userdto', user);
+    const captcha = await this.redisService.get(`captcha_${user.email}`);
+    if (!captcha) {
+      throw new HttpException('验证码已过期', HttpStatus.BAD_REQUEST);
+    }
+
+    if (captcha !== user.captcha) {
+      throw new HttpException('验证码错误', HttpStatus.BAD_REQUEST);
+    }
+
+    const foundUser = await this.userRepository.findOne({
+      where: { username: user.username },
+    });
+
+    if (foundUser) {
+      throw new HttpException('用户名已存在', HttpStatus.BAD_REQUEST);
+    }
+
+    const newUser = new User();
+    newUser.username = user.username;
+    newUser.password = user.password;
+    newUser.email = user.email;
+    newUser.nickName = user.nickName;
+    console.log('newUser aaaaddd');
+    try {
+      console.log('res aaaaddd');
+      await this.userRepository.save(newUser);
+
+      return '成功';
+    } catch (error) {
+      this.logger.error(error, UserService);
+      throw new HttpException('注册失败', HttpStatus.BAD_REQUEST);
+    }
+  }
+}
